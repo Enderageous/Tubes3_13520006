834560032131c0cf6b6f45d841036835245fae42
VionieNovencia <16520188@std.stei.itb.ac.id>
2022-04-29 00:42:02 +0700

    add post( belum work)

--- src/frontend/src/pages/TambahPenyakit.js
+++ src/frontend/src/pages/TambahPenyakit.js
@@ -1,73 +1,95 @@
-import { Component, useState } from "react";
-import { axios } from "axios";
+import { component, useState, useRef } from "react";
+import axios from "axios";
+import { Button } from "@mui/material";
 
-class TambahPenyakit extends Component {
-  state = {
-    name: "",
-  };
+function TambahPenyakit() {
+  const [fileChoosen, setFileChoosen] = useState(false);
+  const [dnaSequence, setDnaSequence] = useState();
+  const [diseaseName, setDiseasesName] = useState("");
 
-  setnama(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const name = { name: this.state.name };
+    // axios
+    //   .post("https://enigmatic-brook-59106.herokuapp.com/api/disease", {
+    //     diseaseName,
+    //     dnaSequence,
+    //   })
+    //   .then((res) => console.log("Posting data", res))
+    //   .catch((error) => console.log(error));
 
-    axios
-      .post(`https://jsonplaceholder.typicode.com/users`, { name })
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-      });
+    axios({
+      url: "https://enigmatic-brook-59106.herokuapp.com/api/disease",
+      data: {
+        diseaseName,
+        dnaSequence,
+      },
+      headers: { "Access-Control-Allow-Origin": "*" },
+      method: "post",
+    })
+      .then((res) => console.log("Posting data", res))
+      .catch((error) => console.log(error));
   };
 
-  onChange(e) {
-    let files = e.target.files;
-    let reader = new FileReader();
-    reader.readAsDataURL(files[0]);
+  const onChange = (e) => {
+    if (e.target.files[0]) {
+      setFileChoosen(true);
+      setDnaSequence(e.target.files[0].name);
+    } else {
+      setFileChoosen(false);
+    }
+  };
 
-    reader.onload = (e) => {
-      console.warn("data uploaded ", e.target.result); //checking only, will be deleted
-    };
-  }
-  render() {
-    return (
-      <div>
-        <h1 className="subtitle"> Tambah Penyakit</h1>
-        <div className="tambahPenyakit">
-          <div className="twocolumn">
-            <p>Nama Pengguna:</p>
-            <form>
-              <input
-                className="twoInput"
-                type="text"
-                name="name"
-                onChange={this.setnama}
-                placeholder="nama..."
-              />
-            </form>
-          </div>
-          {/* <div className="twocolumn">
-            <p>Sequence DNA:</p>
+  return (
+    <div>
+      <h1 className="subtitle"> Tambah Penyakit</h1>
+      <div className="tambahPenyakit">
+        <div className="twocolumn">
+          <p>Nama Penyakit:</p>
+          <form>
             <input
-              className="uploadButtonPenyakit"
+              id="penyakit"
+              className="twoInput"
+              type="text"
+              name="diseaseName"
+              value={diseaseName}
+              onChange={(e) => setDiseasesName(e.target.value)}
+              placeholder="penyakit..."
+            />
+          </form>
+        </div>
+        <div className="twocolumn">
+          <p>Sequence DNA:</p>
+          <Button
+            style={{
+              borderRadius: 5,
+              backgroundColor: "white",
+              fontSize: "20px",
+              color: "grey",
+            }}
+            variant="contained"
+            component="label"
+          >
+            {fileChoosen ? `${dnaSequence}` : "Upload file"}
+            <input
+              id="dna"
               type="file"
-              name="sequence_dna"
-              onChange={(e) => this.onChange(e)}
+              name="dnaSequence"
+              value={dnaSequence}
+              onChange={(e) => setDnaSequence(e.target.value)}
+              hidden
             ></input>
-          </div> */}
-          <div className="onecolumn">
-            <form onSubmit={this.handleSubmit}>
-              <button className="submitButton" type="submit">
-                Submit
-              </button>
-            </form>
-          </div>
+          </Button>
+        </div>
+        <div className="onecolumn">
+          <form onSubmit={handleSubmit}>
+            <button className="submitButton" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default TambahPenyakit;
