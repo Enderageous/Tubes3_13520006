915230a920043ef1e2403ecb86a923112d374413
VionieNovencia <16520188@std.stei.itb.ac.id>
2022-04-29 13:50:29 +0700

    update post tesdna

--- src/frontend/src/pages/TesDNA.js
+++ src/frontend/src/pages/TesDNA.js
@@ -2,25 +2,42 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Button } from "@mui/material";
 
-function TesDNA() {
+const TesDNA = () => {
   const [diseases, setDiseases] = useState([]);
+  const [patientName, setPatientName] = useState("");
+  const [diseaseId, setDiseasesId] = useState("");
   const [fileChoosen, setFileChoosen] = useState(false);
-  const [currFile, setCurrFile] = useState();
+  const [dnaFiles, setDnaFiles] = useState(null);
 
   const onChange = (e) => {
     if (e.target.files[0]) {
       setFileChoosen(true);
-      setCurrFile(e.target.files[0].name);
+      setDnaFiles(e.target.files[0]);
     } else {
       setFileChoosen(false);
     }
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("disease_id", diseaseId);
+    formData.append("patient_name", patientName);
+    formData.append("dna_file", dnaFiles);
+
+    axios({
+      url: "https://enigmatic-brook-59106.herokuapp.com/api/prediction",
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+      method: "post",
+    })
+      .then((res) => alert("Data prediksi berhasil ditambahkan"))
+      .catch((error) => console.log(error));
+  };
   useEffect(() => {
     axios
       .get(`https://enigmatic-brook-59106.herokuapp.com/api/disease`)
       .then((res) => {
-        console.log(res.data);
         setDiseases(res.data);
       })
       .catch((error) => console.log(error));
@@ -28,67 +45,64 @@ function TesDNA() {
 
   return (
     <div className="body">
-      <h1 className="subtitle">Tes DNA</h1>
-      <div className="tesDNA">
-        <div className="threecolumn">
-          <p>Nama Pengguna:</p>
-          <form>
+      <form onSubmit={handleSubmit}>
+        <h1 className="subtitle">Tes DNA</h1>
+        <div className="tesDNA">
+          <div className="threecolumn">
+            <p>Nama Pengguna:</p>
             <input
+              id="nama"
               className="threeInput"
               type="text"
-              name="nama"
+              name="patientName"
+              onChange={(e) => setPatientName(e.target.value)}
               placeholder="<pengguna>"
             />
-          </form>
-        </div>
-        <div className="threecolumn">
-          <p>Sequence DNA:</p>
-          <Button
-            variant="contained"
-            component="label"
-            style={{
-              borderRadius: 5,
-              backgroundColor: "white",
-              fontSize: "20px",
-              color: "grey",
-            }}
-          >
-            {fileChoosen ? `${currFile}` : "Upload file"}
-            <input
-              id="dnaSequence"
-              type="file"
-              name="sequence_dna"
-              onChange={onChange}
-              hidden
-            ></input>
-          </Button>
-          <br></br>
-          <br />
-          <button className="submitButton1" type="submit" color="green">
-            Submit
-          </button>
-        </div>
-        <div className="threecolumn">
-          <p>Prediksi Penyakit:</p>
-          <form>
-            {/* <input
-              className="threeInput"
-              type="text"
-              name="nama"
-              placeholder="<penyakit>"
-            /> */}
-            <select>
+          </div>
+          <div className="threecolumn">
+            <p>Sequence DNA:</p>
+            <Button
+              variant="contained"
+              component="label"
+              style={{
+                borderRadius: 5,
+                backgroundColor: "white",
+                fontSize: "20px",
+                color: "grey",
+              }}
+            >
+              {fileChoosen ? `${dnaFiles.name}` : "Upload file"}
+              <input
+                id="dna"
+                type="file"
+                name="dnaSequence"
+                onChange={(e) => onChange(e)}
+                hidden
+              ></input>
+            </Button>
+            <br></br>
+            <br />
+            <button className="submitButton1" type="submit" color="green">
+              Submit
+            </button>
+          </div>
+          <div className="threecolumn">
+            <p>Prediksi Penyakit:</p>
+            <select
+              name="diseaseId"
+              onChange={(e) => setDiseasesId(e.target.value)}
+            >
               {diseases.map((disease) => (
-                <option key={disease.id} value={disease.disease_id}>
+                <option key={disease.id} value={disease.id}>
                   {disease.disease_name}
                 </option>
               ))}
             </select>
-          </form>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
-}
+};
 
 export default TesDNA;
