a798bf4044fa25a07e053062c87373def47007e1
Enderageous <72645928+Enderageous@users.noreply.github.com>
2022-04-29 10:46:55 +0700

    add Boyer-Moore

--- src/backend/BMS.go
+++ src/backend/BMS.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+// Algoritma Utama Boyer-Moore
+func BMS(input []byte, disease []byte) bool {
+	last := lastOcc(disease)
+	m := len(disease)
+	n := len(input)
+	i := m-1
+	j := m -1
+	found := false
 
-func BMS() {
-	fmt.Println("Hello, World!")
+	if i <= n-1 {
+		for i <= n-1 && !found {
+			if input[i] == disease[j] {
+				if j == 0 {
+					found = true
+				} else {
+					i--
+					j--
+				}
+			} else {
+				jump := last[input[i]]
+				if (j < 1+jump) {
+					i = i + m - j
+				} else {
+					i = i + m - (1 + jump)
+				}
+				j = m - 1
+			}
+		}
+	}
+
+	return found
+}
+
+// Fungsi untuk membentuk sebuah array yang berisi kemunculan terakhir setiap karakter ASCII dalam penyakit
+func lastOcc(disease []byte) [91]int {
+	var last [91]int
+	i := 0
+
+	for i < 91 {
+		last[i] = -1
+		i++
+	}
+
+	i = 0
+	for i < len(disease) {
+		last[disease[i]] = i
+		i++
+	}
+
+	return last
+}
+
+// Fungsi untuk menggunakan Algoritma Boyer-Moore
+func mainBMS(Input string, Disease string) bool {
+	var result bool
+
+	InputByte := []byte(Input)
+	DiseaseByte := []byte(Disease)
+
+	result = BMS(InputByte, DiseaseByte)
+	return result
 }
