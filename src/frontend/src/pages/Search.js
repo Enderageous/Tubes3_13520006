a41797d2f25a939350055b271334e9b219dac00a
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-04-29 09:01:44 +0700

    add search functionality

--- src/frontend/src/pages/Search.js
+++ src/frontend/src/pages/Search.js
@@ -1,62 +1,80 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { Box } from "@mui/material";
-class Search extends React.Component {
-  state = {
-    diseases: [],
-    dates: [],
-    names: [],
-    results: [],
-  };
+import { Box } from "@mui/material"
 
-  componentDidMount() {
-    axios
-      .get(`https://enigmatic-brook-59106.herokuapp.com/api/prediction`)
-      .then((res) => {
-        const diseases = res.data;
-        const dates = res.data;
-        const names = res.data;
-        const results = res.data;
-        this.setState({ diseases, dates, names, results });
-      })
-      .catch((error) => console.log(error));
+const formatDate = (date) => {
+  // change format from yyyy-mm-dd to dd MONTHNAME yyyy
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dateObj = new Date(date);
+  const day = dateObj.getDate();
+  const monthIndex = dateObj.getMonth();
+  const year = dateObj.getFullYear();
+  return `${day} ${monthNames[monthIndex]} ${year}`;
+};
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleChange = (e) => {
+    setSearch(e)
   }
 
-  setSearch(event) {
-    this.setState({ value: event.target.value });
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios(
+        `https://enigmatic-brook-59106.herokuapp.com/api/prediction?q=${search}`
+      );
+      setResults(res.data);
+    };
+    fetchData();
+  }, [search]);
 
-  render() {
     return (
       <div>
         <h1 className="subtitle"> Search</h1>
         <div className="search">
-          <form>
-            {
               <input
                 className="searchInput"
                 type="text"
                 name="penyakit"
                 placeholder="Search..."
-                value={this.state.value}
-                onChange={this.setSearch}
+                onKeyDown={
+                  (e) => {
+                    if (e.key === "Enter") {
+                      setSearch(e.target.value)
+                    }
+                  }
+                }
               />
-            }
-          </form>
         </div>
         <table>
-          {this.state.diseases.map((disease) => (
-            <Box className="searchResult">
-              <tr key={disease.id}>
-                {disease.date} - {disease.patient_name} - {disease.disease_name}
-                - {disease.result ? "True" : "False"}
-              </tr>
-            </Box>
-          ))}
+          {results.map((disease) => {
+            return (
+              <Box className="searchResult">
+                <tr key={disease.id}>
+                  {formatDate(disease.date)} - {disease.patient_name} - {disease.disease_name}
+                  - {disease.result ? "True" : "False"}
+                </tr>
+              </Box>
+            )
+          })}
         </table>
       </div>
     );
-  }
 }
 
 export default Search;
